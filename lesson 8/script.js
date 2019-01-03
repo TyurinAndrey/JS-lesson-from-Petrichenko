0ed8e544f457834a9b7e55bff5a44483b20cdbd7
Andrey <moloh247@mail.ru>
2019-01-03 12:04:46 +0300

    Создание восьмого урока

---
+++ lesson 8/script.js
@@ -0,0 +1,31 @@
+// 'use strict';
+
+// let arr = ("plum.png", "orange.jpg", "apple.bmp");
+// console.log(arr[0]);
+// let answer = +prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + "-object");
+
+// console.log(4 + + "-object");
+
+let incr = 10;
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+
+let isChecked = false;
+    isClose = false;
+
+console.log(isChecked && isClose);
+console.log(isChecked || isClose);
+console.log(isChecked || !isClose);
